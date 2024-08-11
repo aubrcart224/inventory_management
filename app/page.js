33af4a49eb63768716c70793b8f9ad7f6f513d6c
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-08-10 23:54:29 -0400

    more handles

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { collection, doc, Firestore, query } from "firebase/firestore"; // check for error later 
+import { collection, doc, firestore, getDoc, query, setDoc } from "firebase/firestore"; // check for error later 
 import { Box, Typography } from "@mui/material";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
   const [itemName, setItemName] = useState("");
 
   const updateInventory = async () => {
-    const snapshot = query(collection(Firestore, "inventory"));
+    const snapshot = query(collection(firestore, "inventory"));
     const docs = await snapshot.get();
     const inventorylist = [];
 
@@ -26,21 +26,72 @@ export default function Home() {
     setInventory(inventorylist);
   }
 
+  //add items
+  const addItem = async (name) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);   
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+    await updateInventory();
+  
+  }
+
+  //remove items
+  const removeItem = async (name) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);   
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+    await updateInventory();
+  
+    useEffect(() => {
+      updateInventory(); // delete or keep this later, might error. 
+    }, []);
+
+  }
+
   useEffect(() => {
     updateInventory();
   }, []);
 
+
+  // handle open and close 
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  
   return (
     <Box>
       <Typography variant="h1">Inventory Managment</Typography>
       {
         inventory.forEach((item)=> {
-        return(<>
+        return(
+          <Box>
         {item.name}
         {item.count}
-      </> )
+      </Box>
+      )
       })
     }
     </Box>
   )
+
 }
