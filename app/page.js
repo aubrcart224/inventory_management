b745245470295e4e0df46de2729cd2f453e27244
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-08-10 23:04:34 -0400

    more

--- app/page.js
+++ app/page.js
@@ -1,7 +1,46 @@
+'use client'
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { collection, doc, Firestore, query } from "firebase/firestore"; // check for error later 
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   
-  return<>hello world</>;
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
 
+  const updateInventory = async () => {
+    const snapshot = query(collection(Firestore, "inventory"));
+    const docs = await snapshot.get();
+    const inventorylist = [];
+
+    docs.forEach(doc => {
+      inventorylist.push({
+      name: doc.id,
+      ...doc.data(),
+      });
+
+    })
+    setInventory(inventorylist);
+  }
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Managment</Typography>
+      {
+        inventory.forEach((item)=> {
+        return(<>
+        {item.name}
+        {item.count}
+      </> )
+      })
+    }
+    </Box>
+  )
 }
